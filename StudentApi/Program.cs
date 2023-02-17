bce36eb1e2aeb840a00463750e88069e47c511f2
lexxA87 <alex.o.nikonov@gmail.com>
2023-02-17 19:22:23 +0400

    Added DBContext and Migrations and connect to SqlServer

--- StudentApi/Program.cs
+++ StudentApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using StudentApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Dependencies Injection of DbContext Class
+builder.Services.AddDbContext<APIDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
 var app = builder.Build();
 
